661efd3f43ec7ae6f9a5e1b589f593c03bf3a61c
iblas <ibrahimiblas@gmail.com>
2022-07-13 01:18:50 +0100

    lazy loading fixed, mobile responsive started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import CountriesList from "./components/CountriesList";
 import FilterCountry from "./components/FilterCountry";
 import CountryProvider from "./components/store/CountryProvider";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
 const SingleCountry = React.lazy(() => import("./components/SingleCountry"));
 function App() {
   return (
     <CountryProvider>
-      <Switch>
-        <Route path={"/"} exact>
-          <FilterCountry />
-          <div>
-            <CountriesList />
-          </div>
-        </Route>
-        <Route path={"/detail/:countryId"}>
-          <SingleCountry />
-          {/* <div>hello</div> */}
-        </Route>
-      </Switch>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <Route path={"/"} exact>
+            <FilterCountry />
+            <div>
+              <CountriesList />
+            </div>
+          </Route>
+          <Route path={"/detail/:countryId"}>
+            <SingleCountry />
+            {/* <div>hello</div> */}
+          </Route>
+        </Switch>
+      </Suspense>
     </CountryProvider>
   );
 }
